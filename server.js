852c71c2f595f4f0ff2bc86bd06d027fefe900d3
gnsnghm <41932520+gnsnghm@users.noreply.github.com>
2023-09-18 13:46:46 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const accountsControler = require('./controllers/accountsController')
+require('dotenv').config();
+
+// Setting middleware
+const cors = require('cors'); // Cross Origin Resouce Sharing を有効にする
+const bodyParser = require('body-parser'); // レスポンスのフォーマットを変換する
+const morgan = require('morgan') // HTTPリクエストロガー
+const helmet = require('helmet') // Cross-Site-Scripting(XSS)のような攻撃を防ぐ
+
+// knex を使って database に接続する
+var db = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'ae-db',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+  }
+});
+
+// Middleware
+const whitelist = ['http://localhost:3001'];
+const corsOption = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+app.use(helmet());
+app.use(cors(corsOption));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+// Router
+app.get('/', (req, res) => res.send('Server is running.'));
+app.get('/crud', (req, res) => accountsControler.getData(req, res, db));
+app.post('/crud', (req, res) => accountsControler.postData(req, res, db));
+app.put('/crud', (req, res) => accountsControler.putData(req, res, db));
+app.delete('/delete', (req, res) => accountsControler.delData(req, res, db));
+
+// Connect
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`port ${process.env.PORT} || 3000`)
+});
+
+
